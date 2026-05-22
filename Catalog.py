bd4b784c479640356fdd0168617db8290d37053c
Isar <isar@kavlie.no>
2026-05-22 17:36:29 +0200

    renames file, adds cherrypy branch

--- Catalog.py
+++ Catalog.py
@@ -1,7 +1,10 @@
 import cherrypy
+import json
+import time
 import threading
 class Catalog:
     def __init__(self):
         threading.Thread(target=self._cleanup_loop, daemon=True).start()
-        
     def _cleanup_loop(self):
+        pass
+    
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cherrypy
 from constants import PORT_NUMBER, HOST_NAME
 from EventLog import EventLog
+from Catalog import Catalog
 # from SmartHomeSensorService import SmartHomeSensorService
 
 if __name__ == "__main__":
@@ -13,6 +14,7 @@ if __name__ == "__main__":
 
     # service mounting section
     cherrypy.tree.mount(EventLog(), '/log', conf)
+    cherrypy.tree.mount(Catalog(), "/Catalog", conf)
     # cherrypy.tree.mount(SmartHomeSensorService(), '/sensor', conf)
     
     # host config

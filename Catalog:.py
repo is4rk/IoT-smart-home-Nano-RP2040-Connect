25db231b70ac00d8cc8b9029a3a06e9fca4af67a
Isar <isar@kavlie.no>
2026-05-22 17:31:08 +0200

    catalog new files

---
+++ Catalog:.py
@@ -0,0 +1,7 @@
+import cherrypy
+import threading
+class Catalog:
+    def __init__(self):
+        threading.Thread(target=self._cleanup_loop, daemon=True).start()
+        
+    def _cleanup_loop(self):

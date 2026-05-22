152723465a3e9312752177d6b7d5b44454725c07
Isar <isar@kavlie.no>
2026-05-22 18:25:51 +0200

    adds get post delete~

--- Catalog.py
+++ Catalog.py
@@ -6,16 +6,25 @@ class Catalog:
     def __init__(self):
         threading.Thread(target=self._cleanup_loop, daemon=True).start()
         cat_dict=json.loads()
-        
-        json_file_name="catalog.json"
+        self.json_file_name="catalog.json"
+
     def _cleanup_loop(self):
         pass
     def GET(self, *path, **query):
-        pass
+        with open(self.json_file_name, "r") as f:
+            return json.loads(f)
     def POST(self, *path, **query):
-        pass
+        body = json.loads(cherrypy.request.body.read())
+        with open(self.json_file_name, "w") as f:
+            json.dumps(f, body) #qualcosa
     def PUT(self, *path, **query):
         pass
-    def DELETE(self, *path, **query):
-        pass
-    
+    def DELETE(self, *path, **query): #to finish at home
+        body = json.load(cherrypy.request.body.read())
+        
+        with open(self.json_file_name, "r") as f:
+            data = json.load(f)
+        id_to_delete=body["ID"]
+        del data[id_to_delete] #TO CONTROL
+        with open(self.json_file_name, "w") as f:
+            pass

9996b00490d102bb7e1d4dd6b11a70c4c9d85635
is4rk <is4r@protonmail.com>
2026-06-09 20:13:21 +0200

    fixes list use instead of dictionray in catalog

--- Catalog.py
+++ Catalog.py
@@ -20,11 +20,10 @@ class Catalog:
             with open(self.json_file_name, "r") as f:
                 data=json.load(f) #load on data the catalog.json
             for id, device in list(data["devices"].items()): 
-                if now - device["element"]["time"] > 120: # for each device, it checks if the devices has not been updated in 120 seconds. In that case, it delate all the deprecated devices
+                if now - device.get("time", 0) > 120: # for each device, it checks if the devices has not been updated in 120 seconds. In that case, it delate all the deprecated devices
                     del data["devices"][id] 
             for id, service in list(data["services"].items()): # It works in the same way of the previuos for statement, but for services
-                if now - service["element"]["time"] > 120:
-                    del data["services"][id]
+                if now - service.get("time", 0) > 120:                    del data["services"][id]
             with open(self.json_file_name, "w") as f: # After any changes, overwrites the old catalog with the new one
                 json.dump(data, f)
 
@@ -32,14 +31,13 @@ class Catalog:
         with open(self.json_file_name, "r") as f:
             catalog = json.load(f)
         if len(path)==0:
-            return json.dump(catalog)
+            return json.dumps(catalog)
         if len(path) == 1 and path[0]=="broker":
-            return json.dump(catalog.get("broker"))        
+            return json.dumps(catalog.get("broker"))        
         if len(path) == 1 and path[0] in ["devices", "services"]:
-            return json.dump(catalog.get(path[0]))
+            return json.dumps(catalog.get(path[0]))
         if len(path) == 2 and path[0] in ["devices", "services"]:
-            items = catalog.get(path[0], []) #gets all devices or services, if directory is not found returns empty list []
-            match = next((x for x in items if x["id"]==path[1]), None)
+            match = catalog[path[0]][path[1]]
             if match is None:
                 raise cherrypy.HTTPError(404, f"{path[1]} not found")
             return json.dumps(match)
@@ -52,38 +50,38 @@ class Catalog:
         with self.lock:
             with open(self.json_file_name, "r") as f: # it loads on data the content of catalog.json
                 catalog = json.load(f)
-            items = catalog.get(path[0], [])
-            match = next((x for x in items if x["id"]==body["id"]), None) #iterates to see if an element with the given id already exists
-            if match is not None:
+            if path[1] in catalog[path[0]]: #TODO: check if correct, also in PUT
                 raise cherrypy.HTTPError(409, f"{body['id']} already exists")
-            body["time"]=time.time() #sets time to insertion time
-            catalog[path[0]].append(body) #appends to the catalog list, to then get converted
-            with open(self.json_file_name, "w") as f:
-                json.dump(catalog, f) #saves to catalog
-            return json.dumps(body) #returns the inserted body, with updated time stamp
+            else:
+                body["time"]=time.time() #sets time to insertion time
+                catalog[path[0]][body["id"]]=body
+                with open(self.json_file_name, "w") as f:
+                    json.dump(catalog, f) #saves to catalog
+                return json.dumps(body) #returns the inserted body, with updated time stamp
+            
             
     def PUT(self, *path, **query):  # It handles HTTP PUT requests used to update or refresh the time of an existing device/service registration.
         body = cherrypy.request.body.read()
         body = json.loads(body) if body else None
         if len(path)!=2 or path[0] not in ["services", "devices"]:
             raise cherrypy.HTTPError(400, "Invalid path")
+        if body is None:
+            raise cherrypy.HTTPError(404, f"{path[1]} not found")
         with self.lock:
             with open(self.json_file_name, "r") as f:
                 catalog = json.load(f)
-            items = catalog.get(path[0], [])
-            match = next((x for x in items if x["id"]==path[1]), None)
-            if match == None:
-                if body is None:
-                    raise cherrypy.HTTPError(404, f"{path[1]} not found")
+            if path[1] in catalog[path[0]]:
+                
                 body["time"]=time.time() #sets time to insertion time
-                catalog[path[0]].append(body) #appends to the catalog list, to then get converted
+                catalog[path[0]][body["id"]]=body 
                 with open(self.json_file_name, "w") as f:
                     json.dump(catalog, f) #saves to catalog
                 return json.dumps(body)
-            match["time"] = time.time()
+            body["time"] = time.time()
+            catalog[path[0]][body["id"]]=body
             with open(self.json_file_name, "w") as f:
                 json.dump(catalog, f)
-            return json.dumps(match)
+            return json.dumps(body)
 
     # TODO: make body use path to delete
     def DELETE(self, *path, **query): # It handles HTTP DELETE requests used to remove a device or service from the Catalog
@@ -93,12 +91,13 @@ class Catalog:
             with open(self.json_file_name, "r") as f: 
                 catalog = json.load(f)    
             if len(path) ==2:
-                items = catalog.get(path[0], [])
-                match = next((x for x in items if x["id"]==path[1]), None)
-                if match is None:
+                if path[1] not in catalog[path[0]]:
                     raise cherrypy.HTTPError(404, f"{path[1]} not found")
-                catalog[path[0]].remove(match)
-            catalog[path[0]]=[]
+                del catalog[path[0]][path[1]]
+            elif len(path)==1:
+                catalog[path[0]] = []
+            elif len(path)==1:
+                catalog={}
                 
             with open(self.json_file_name, "w") as f:
                     json.dump(catalog, f)

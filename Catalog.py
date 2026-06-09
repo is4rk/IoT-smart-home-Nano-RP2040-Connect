1d0649c1702ab13353319d4d80bf0168f2224f20
is4rk <is4r@protonmail.com>
2026-06-09 23:54:23 +0200

    prev

--- Catalog.py
+++ Catalog.py
@@ -221,6 +221,7 @@ if __name__ == '__main__':
         }
     }
     cherrypy.tree.mount(Catalog(), '/', conf)
-    cherrypy.config.update({'server.socket_port': 8080})
+    # cherrypy.config.update({'server.socket_port': 8080})
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8080})
     cherrypy.engine.start()
     cherrypy.engine.block()

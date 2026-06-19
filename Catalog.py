e84808cea1e0ccd396d7e95509ace3e5b32a7d7f
Isar <isar@kavlie.no>
2026-06-19 16:26:08 +0200

    fix catalog

--- Catalog.py
+++ Catalog.py
@@ -219,8 +219,12 @@ if __name__ == '__main__':
             'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
         }
     }
+    
     cherrypy.tree.mount(Catalog(), '/catalog', conf)
     # cherrypy.config.update({'server.socket_port': 8080})
-    cherrypy.config.update({'server.socket_host': 'localhost', 'server.socket_port': constants.PORT_NUMBER})
+    cherrypy.config.update({
+            'server.socket_host': '0.0.0.0', 
+            'server.socket_port': constants.PORT_NUMBER
+        })    
     cherrypy.engine.start()
     cherrypy.engine.block()

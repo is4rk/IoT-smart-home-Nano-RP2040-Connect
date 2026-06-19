618a47cfb294a2e70ab684c2880944cbba8d2e4e
Isar <isar@kavlie.no>
2026-06-19 16:19:38 +0200

    fix catalog

--- CatalogClient.py
+++ CatalogClient.py
@@ -8,37 +8,37 @@ class CatalogClient:
         return (requests.get(self.url)).json()
 
     def get_devices(self):
-        url = self.url+"/devices"
+        url = self.url+"/catalog/devices"
         return (requests.get(url)).json()
 
     def get_device(self, id):
-        url = self.url+"/devices/"+str(id)
+        url = self.url+"/catalog/devices/"+str(id)
         return (requests.get(url)).json()
     
     def get_services(self):
-        url = self.url + "/services"
+        url = self.url + "/catalog/services"
         return requests.get(url).json()
 
     def get_service(self, id):
-        url = self.url + "/services/" + str(id)
+        url = self.url + "/catalog/services/" + str(id)
         return requests.get(url).json()
 
     def get_broker(self):
-        url = self.url+"/broker"
+        url = self.url+"/catalog/broker"
         return (requests.get(url)).json()
 
     def register_device(self, payload):
-        url = self.url+"/devices"
+        url = self.url+"/catalog/devices"
         return (requests.post(url, json=payload)).json()
     
     def register_service(self,payload):
-        url = self.url+"/services"
+        url = self.url+"/catalog/services"
         return (requests.post(url, json=payload)).json()
 
     def refresh_device(self, id, payload):
-        url = self.url+"/devices/"+str(id)
+        url = self.url+"/catalog/devices/"+str(id)
         return (requests.put(url, json=payload)).json()
  
     def refresh_service(self, id, payload):
-        url = self.url+"/services/"+str(id)
+        url = self.url+"/catalog/services/"+str(id)
         return (requests.put(url, json=payload)).json()
